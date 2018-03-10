d51667de905ba7b55a0c50a7f21fbdc64aeed0ec
Carles Andres <carlos.andres.c@gmail.com>
2018-03-10 23:57:35 +0000

    Added a title, Removed paste to textmarkr

--- dirtozip/popup.js
+++ dirtozip/popup.js
@@ -7,30 +7,22 @@ function allUrls(callback){
     currentWindow: true
   };
 
-    chrome.tabs.query(queryInfo, function(tabs){
-        var getMarkdownLink = function(tab){
-            return '[' + tab.title + '](' + tab.url + ')';
-        };
-        var urls = tabs.map( getMarkdownLink );
-        var urlList = urls.join('\n');
-        var url = 'https:www.textmarkr.com/paste?title=Bookmarks&text=' + encodeURIComponent(urlList);
+  chrome.tabs.query(queryInfo, function(tabs){
+    var getMarkdownLink = function(tab){
+      return '[' + tab.title + '](' + tab.url + ')';
+    };
+    var urlList = tabs.map( getMarkdownLink ).join('\n');
+    renderStatus(urlList);
 
-        if (url.length > 2000){
-            // renderStatus('-----' + url.length + '----');
-            renderStatus(urlList);
-        } else {
-            renderStatus(url);
-        }
-
-        // try {
-        //     document.execCommand('copy');
-        //     const url = 'https:www.textmarkr.com/paste?title=Bookmarks&text=' + encodeURIComponent(urlList);
-        //     renderStatus(url);
-        // } catch (e) {
-        //     renderStatus(urlList);
-        //     console.log('Imposible to copy');
-        // }
-    });
+    // try {
+    //     document.execCommand('copy');
+    //     const url = 'https:www.textmarkr.com/paste?title=Bookmarks&text=' + encodeURIComponent(urlList);
+    //     renderStatus(url);
+    // } catch (e) {
+    //     renderStatus(urlList);
+    //     console.log('Imposible to copy');
+    // }
+  });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
