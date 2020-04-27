af055ee414bccd1858b99cce4ee6b7e8e4d9a599
Carles Andres <carlos.andres.c@gmail.com>
2020-04-27 19:49:25 +0100

    Close tabs functionality

--- source/popup.js
+++ source/popup.js
@@ -7,16 +7,20 @@ const copyToClipboard = str => {
   document.body.removeChild(el);
 };
 
-const closeTab = tabId => chrome.tabs.remove(tabId);
+const closeTab = tabId => {
+  chrome.tabs.remove(tabId);
+}
 
 const makeTabElement = tab => {
   return (
   `<li>
     <div class="title">${tab.title}</div>
-    <div class="actions"><button class="close" id="tab-${tab.id}">Close tab</button></div>
+    <div class="actions"><button class="close" id="tab-${tab.id}">Close</button></div>
     </li>`
   );
 };
+
+let filterValue = '';
 
 const attachActionsToList = () => {
   const closeButtons = document.querySelectorAll('#list button.close');
@@ -24,32 +28,47 @@ const attachActionsToList = () => {
     const id = parseInt(b.id.replace('tab-',''), 10);
     b.onclick = () => closeTab(id)
   });
+
+  const filter = document.getElementById('filter');
+  filter.addEventListener('input', onUpdateFilter);
 }
 
-function allUrls(){
+const onUpdateFilter = function() {
+  filterValue = (event.target.value || '').trim();
+  populateList();
+};
+
+chrome.tabs.onRemoved.addListener(populateList);
+
+function populateList(){
   const queryInfo = { currentWindow: true };
 
   chrome.tabs.query(queryInfo, function(tabs){
-    const getMarkdownLink = tab => `[${tab.title}](${tab.url})`;
-    const markdown = tabs.map(getMarkdownLink).join('\n');
 
-    let linkList = tabs.map(makeTabElement).join('\n');
+    const filteredTabs = tabs.filter(tab => !filterValue || tab.title.match(filterValue));
+
+    let linkList = filteredTabs.map(makeTabElement).join('\n');
     linkList = `<ul>${linkList}</ul>`;
     document.getElementById('list').innerHTML = linkList;
 
     setTimeout( attachActionsToList, 0);
 
-    const button = document.querySelector('#copy-to-clipboard');
+    const filter = document.getElementById('filter');
+    filter.focus;
 
-    try {
-      copyToClipboard(markdown);
-      button.style = "display:block";
-    } catch (e) {
-      button.style = "display:hidden";
-    }
+    // const getMarkdownLink = tab => `[${tab.title}](${tab.url})`;
+    // const markdown = tabs.map(getMarkdownLink).join('\n');
+    // const button = document.querySelector('#copy-to-clipboard');
+    //
+    // try {
+    //   copyToClipboard(markdown);
+    //   button.style = "display:block";
+    // } catch (e) {
+    //   button.style = "display:hidden";
+    // }
   });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  allUrls();
+  populateList();
 });
